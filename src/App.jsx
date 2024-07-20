eca4cf6cb160c57f934d0bba8a2ef44c74cc6247
Raju Achhami <achhami.raju55@gmail.com>
2024-07-20 19:01:14 +0545

    ch

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [color, setColor] = useState("olive");
 
+  const changeColor = () => {};
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className=" flex justify-center pt-4">
+        <div className="bg-white px-3 py-2 rounded-lg flex gap-5">
+          <button
+            className="bg-red-500 px-4 rounded text-white"
+            onClick={() => setColor("red")}
+          >
+            Red
+          </button>
+          <button
+            className="bg-green-500 px-4 rounded text-white"
+            onClick={() => setColor("green")}
+          >
+            Green
+          </button>
+          <button
+            className="bg-blue-500 px-4 rounded text-white"
+            onClick={() => setColor("blue")}
+          >
+            Blue
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
